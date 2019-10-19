265f0ff5da1ab14f683c5aeb037ba00d5889a27b
hellno <lol@nope.com>
2019-10-19 20:03:11 +0200

    comment

--- pure-tanks/game.nim
+++ pure-tanks/game.nim
@@ -8,7 +8,8 @@ import types
 import vector
 
 func player_forward(info: UpdateInfo, player: Player): Player =
-  # returns the resulting player, after applying the "forward" command
+  ## returns the resulting Player object,
+  ## after applying the "forward" Command
   let distance = info.config.playerspeed * info.config.timemod * (float) info.delta_t
   let new_position = move(player.position, player.angle, distance)
   return Player(angle: player.angle,
@@ -27,16 +28,18 @@ func get_player(info: UpdateInfo, name: Name): Player =
 
 func handle_move_command(info: UpdateInfo, cmd: Command): GameState =
     ## return GameState after handling exactly one Command
-    let player = info.get_player(cmd.name)
 
     # table mapping Actions to move functions
     # TODO: complete movement functions
-    let move_functions = {
+    const move_functions = {
       forward: player_forward,
       backward: nil,
       left: nil,
       right: nil
     }.toTable
+
+    # find the command-issuing player
+    let player = info.get_player(cmd.name)
 
     # apply relevant move function to command-issuing player only
     func apply_function(p: Player): Player =
@@ -56,14 +59,17 @@ func handle_move_command(info: UpdateInfo, cmd: Command): GameState =
     return new_state
 
 
-
 func update*(info: UpdateInfo, commands: seq[Command]): GameState =
+  ## Progresses GameState one tick (by `delta_time`),
+  ## by executing all player-issued Commands and moving projectiles
 
-  func f(state: GameState, cmd: Command): GameState =
+  func apply_command(state: GameState, cmd: Command): GameState =
+    ## returns the gamestate achieved after executing exactly one command
     let finfo = UpdateInfo(state: state,
                            config: info.config,
                            delta_t: info.delta_t)
     return handle_move_command(finfo, cmd)
 
-  let new_state: GameState = foldl(commands, f(a, b), info.state)
+  # apply all commands in succession, returning the final resulting GameState
+  let new_state: GameState = foldl(commands, apply_command(a, b), info.state)
   return new_state
