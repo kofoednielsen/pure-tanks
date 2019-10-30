049a1a39eadb265bc250e905f726efe8bed38f79
AsbjornOlling <asbjornolling@gmail.com>
2019-10-30 21:17:50 +0100

    split types into groups for readability

--- pure-tanks/types.nim
+++ pure-tanks/types.nim
@@ -1,6 +1,6 @@
+# Geometry
 type
   Angle* = float  #  radians
-  Name* = string
 
   Point* = object
     x*, y*: float
@@ -8,27 +8,34 @@ type
   Segment* = object
     a*, b*: Point
 
-  Rect* = object
-    angle*: Angle
-    pos*: Point
-    width*, height*: int
+  Polygon* = object
+    center*: Point
+    segments*: seq[Segment]
+
+
+# Game objects
+type
+  Name* = string
 
   Player* = object
     kills*, deaths*: int
     name*: Name
-    shape*: Rect
+    shape*: Polygon
 
   Projectile* = object
     owner*: Name
-    shape*: Rect
+    shape*: Polygon
 
-  Map* = seq[Rect]
+  Map* = seq[Polygon]
 
   GameState* = object
     projectiles*: seq[Projectile]
     players*: seq[Player]
     map*: Map
 
+
+# Controls
+type
   Action* = enum
     forward = "forward",
     backward = "backward",
@@ -39,6 +46,9 @@ type
 
   Command* = tuple[name: Name, action: Action]
 
+
+# Internals
+type
   Config* = object
     timemod*: float
     rotationspeed*: float    #  radians/microseconds [-PI;PI]
